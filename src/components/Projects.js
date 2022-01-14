e0f85663cf985ca7ab9273c16f4cc7709d3ec703
sohailbadghisi1 <sohailbadghisi1@gmail.com>
2022-01-14 23:15:57 +0430

    Updates

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -36,16 +36,16 @@ const Projects = () => {
         </Fade>
         <Fade bottom>
           <div className="project">
-            <div className="project-image" style={{background: url('https://i.ibb.co/D7pzgVg/Capture.png')}}></div>
+            <div className="project-image"></div>
             <div className="project-info">
-              <h2>Website for a Cafe</h2>
+              <h2>Chat App</h2>
               <h4>
                 Lorem Ipsum is simply dummy text of the printing and typesetting
                 industry. Lorem Ipsum has been the industry's standard dummy
                 text ever since the 1500s.
               </h4>
               <div className="pr-link">
-                <a href="https://heuristic-shockley-51c9e6.netlify.com/?fbclid=IwAR27fJaLPPBydNz4diTof-R0BBMH9_JpVLkxoE7xE2iqppOIJRnWvsxitWY" className="project-btn">
+                <a href="#" className="project-btn">
                   Visit website
                 </a>
               </div>
@@ -76,7 +76,27 @@ const Projects = () => {
             </div>
           </div>
         </Fade>
-
+        <Fade bottom>
+          <div className="project">
+            <div className="project-image"></div>
+            <div className="project-info">
+              <h2>Chat App</h2>
+              <h4>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s.
+              </h4>
+              <div className="pr-link">
+                <a href="#" className="project-btn">
+                  Visit website
+                </a>
+              </div>
+              <div>
+                <span>Technologies: ReactNative, Firebase </span>
+              </div>
+            </div>
+          </div>
+        </Fade>
       </div>
     </div>
   );
